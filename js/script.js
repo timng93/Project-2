6dafb034fc86892a63b00596c59855822791fb73
Timmy <timgabrielnguyen@gmail.com>
2018-10-21 23:05:14 -0700

    Edit CSS HTML JAVASCRIPT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-  // js code
-
-  console.log("Mewlo world");
+  $("#selector").on("change", function() {
+    const selected = $(this).val();
+    $.ajax({
+      url:
+        "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=d28069e91a3c4589b4bc13f58c599967",
+      method: "GET"
+    }).done(function(data) {
+      $.each(data.results, function(key, value) {
+        $(".top-stories ul").append(
+          "<li>" +
+            "<img src='" +
+            value.multimedia[4].url +
+            "'>" +
+            value.multimedia[4].caption +
+            "</li>"
+        );
+      });
+    });
+  });
 });
